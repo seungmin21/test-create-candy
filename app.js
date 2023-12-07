7d5d32fc17b12afde8f0ca6608ded727bdd2fcb6
ohseungmin <ohmin2886@gmail.com>
2023-12-07 17:48:09 +0900

    component라는 함수는 add함수를 담는 매개입니다.

--- app.js
+++ app.js
@@ -3,3 +3,14 @@ function add(typeName, props, children) {
 }
 console.log(add())
 
+function component() {
+  const List = [];
+  for(let i = 0; i < 1; i++) {
+    const item = add('div', {id:'root'}, ["테스트 중"])
+    List.push(item)
+  }
+  return List
+}
+
+console.log(component())
+
