1e59914ec958db3454e7e21f7e8d1b032943771e
ohseungmin <ohmin2886@gmail.com>
2023-12-07 20:21:16 +0900

    createElement에 대한 동기분의 예제입니다.

--- compo.js
+++ compo.js
@@ -1,3 +1,4 @@
+// 다른 동기분(최성민님)의 예제입니다.
 export function makecompo(fatagId, tagName, id, style, contents){  
   const fatag = document.getElementById(fatagId)
   const tag =  `<${tagName} id:${id} style:${style}>${contents}</${tagName}`
